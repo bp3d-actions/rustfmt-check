992ad47870040e80f2569fb419da2a12704e6660
Yuri Edward <yuri6037@outlook.com>
2023-07-31 18:40:24 +0200

    Whatever

--- src/rustfmt.ts
+++ src/rustfmt.ts
@@ -9,8 +9,7 @@ const rustfmt = async (
   args: string = core.getInput("args"),
   toolchain = core.getInput("toolchain")
 ): Promise<string[]> => {
-  if (!toolchain || toolchain.length == 0)
-    toolchain = "stable";
+  if (!toolchain || toolchain.length == 0) toolchain = "stable";
   output.splice(0, output.length);
   return Cargo.get()
     .then(async (cargo) =>
